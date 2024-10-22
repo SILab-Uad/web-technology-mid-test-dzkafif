de7bf52fea53e13b3419cc65bb042a1298ca5740
dzkafif <muhammaddzakwan4@gmail.com>
2024-10-22 10:04:17 +0700

    Attempt 1 - 10.30

--- script.js
+++ script.js
@@ -8,11 +8,28 @@ const generatePassword = (length, options) => {
     const specialChars = "!@#$%^&*()";
 
     // TODO: Create a variable for the character set based on selected options
+let charSet = '';
+if (options.includeUppercase) charSet += uppercase;
+if (options.includeLowercase) charSet += lowercase;
+if (options.includeNumbers) charSet += numbers;
+if (options.includeSpecialChars) charSet += specialChars;
 
     // TODO: Generate the password based on the selected criteria
+    let password ="";
+    for (let i=0;i<length;i++){
+        const randomIndex = Math.floor(Math.random()*charSet.length);
+        password += charSet[randomIndex];
+    }
     return password;
 };
 
 // TODO: Add event listener to the button to call generatePassword and display the output
-
+document.getElementById('generateBtn').addEventListener('click',()=> {
+    const event = new Event('click');
+document.getElementById('generateBtn').dispatchEvent(event);
+});
 // BONUS: Implement the copy to clipboard functionality
+document.getElementById('copyBtn').addEventListener('click',()=> {
+    const event = new Event('click');
+document.getElementById('copyBtn').dispatchEvent(event);
+});
